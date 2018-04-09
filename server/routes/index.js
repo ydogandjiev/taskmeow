3141796e5319ed4428900a108ef9f09d07c7fff8
Yuri Dogandjiev <yudogan@microsoft.com>
2018-04-08 18:20:23 -0700

    Update boilerplate code

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/message', function(req, res, next) {
-  res.json('Welcome To React');
+  res.json('Welcome To React Cosmos DB');
 });
 
 module.exports = router;
