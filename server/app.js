85a5750c0abddcdb9e495f8136eb7295c86235cf
Yuri Dogandjiev <yudogan@microsoft.com>
2018-04-22 11:37:52 -0700

    Update favicon

--- server/app.js
+++ server/app.js
@@ -10,8 +10,6 @@ const index = require("./routes/index");
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
