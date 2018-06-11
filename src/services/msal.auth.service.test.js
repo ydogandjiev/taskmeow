6b3a6397000b4c757ac7b5b03e75939ba410eb89
Yuri Dogandjiev <yudogan@microsoft.com>
2018-06-10 22:54:41 -0700

    Add MSAL auth service unit tests

---
+++ src/services/msal.auth.service.test.js
@@ -0,0 +1,72 @@
+import * as Msal from "msal";
+jest.mock("msal");
+
+import MsalAuthService from "./msal.auth.service";
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+it("can be constructed", () => {
+  const authService = new MsalAuthService();
+  expect(authService).toBeDefined();
+  expect(Msal.UserAgentApplication.mock.instances.length).toEqual(1);
+});
+
+it("can check for callback", () => {
+  const authService = new MsalAuthService();
+  const app = Msal.UserAgentApplication.mock.instances[0];
+  app.isCallback.mockReturnValue(true);
+
+  expect(authService.isCallback()).toEqual(true);
+  expect(app.isCallback).toHaveBeenCalledTimes(1);
+});
+
+it("can initiate login", () => {
+  const authService = new MsalAuthService();
+  const app = Msal.UserAgentApplication.mock.instances[0];
+  app.loginPopup.mockResolvedValue();
+
+  authService.login();
+  expect(app.loginPopup).toHaveBeenCalledTimes(1);
+});
+
+it("can initiate logout", () => {
+  const authService = new MsalAuthService();
+  const app = Msal.UserAgentApplication.mock.instances[0];
+
+  authService.logout();
+  expect(app.logout).toHaveBeenCalledTimes(1);
+});
+
+it("can get token", done => {
+  const authService = new MsalAuthService();
+  const app = Msal.UserAgentApplication.mock.instances[0];
+  app.acquireTokenSilent.mockResolvedValue("fakeToken");
+
+  authService.getToken().then(token => {
+    expect(token).toEqual("fakeToken");
+    done();
+  });
+
+  expect(app.acquireTokenSilent).toHaveBeenCalledTimes(1);
+});
+
+it("can get user", done => {
+  const mockUser = {
+    name: "fakeFirst fakeLast",
+    given_name: "fakeFirst",
+    oid: "fakeOid"
+  };
+
+  const authService = new MsalAuthService();
+  const app = Msal.UserAgentApplication.mock.instances[0];
+  app.getUser.mockResolvedValue(mockUser);
+
+  authService.getUser().then(user => {
+    expect(user).toEqual(mockUser);
+    done();
+  });
+
+  expect(app.getUser).toHaveBeenCalledTimes(1);
+});
