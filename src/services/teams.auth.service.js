b4ca536a259503bba728875ace1545e499154b25
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2021-02-02 08:09:18 -0800

    Add Teams auth using MSAL 0.1.5

--- src/services/teams.auth.service.js
+++ src/services/teams.auth.service.js
@@ -1,61 +1,48 @@
-import AuthenticationContext from "adal-angular/lib/adal";
 import * as microsoftTeams from "@microsoft/teams-js";
+import * as Msal from "msal";
 
-// An authentication service that uses the ADAL.js and Teams.js library to sign in users with
-// their AAD account. This leverages the AAD v1 endpoint.
+// An authentication service that uses the MSAL.js and Teams.js library to sign in users with
+// their AAD account. This leverages the AAD v2 endpoint.
 class TeamsAuthService {
   constructor() {
     // Initialize the Teams SDK
     microsoftTeams.initialize();
 
-    // Check for any context information supplied via our QSPs
-    const url = new URL(window.location);
-    const params = new URLSearchParams(url.search);
-    const tenantId = params["tenantId"] || "common";
-
-    // TODO: Clear sign in context on user mismatch
-    // const userObjectId = params["userObjectId"];
-
-    const clientId =
-      window.location.hostname === "taskmeow.com"
-        ? "36b1586d-b1da-45d2-9b32-899c3757b6f8"
-        : "ab93102c-869b-4d34-a921-a31d3e7f76ef";
-
-    // Configure ADAL
     this.applicationConfig = {
-      tenant: tenantId,
-      clientId: clientId,
-      endpoints: {
-        api: clientId,
-      },
-      redirectUri: `${window.location.origin}/tab/silent-end`,
-      cacheLocation: "localStorage",
-      navigateToLoginRequestUrl: false,
+      clientId:
+        window.location.hostname === "taskmeow.com"
+          ? "36b1586d-b1da-45d2-9b32-899c3757b6f8"
+          : "ab93102c-869b-4d34-a921-a31d3e7f76ef",
     };
 
-    this.authContext = new AuthenticationContext(this.applicationConfig);
+    this.app = new Msal.UserAgentApplication(
+      this.applicationConfig.clientId,
+      "",
+      null,
+      {
+        redirectUri: `${window.location.origin}/tab/silent-end`,
+      }
+    );
   }
 
   isCallback() {
-    return this.authContext.isCallback(window.location.hash);
+    return this.app.isCallback(window.location.hash);
   }
 
   login() {
     if (!this.loginPromise) {
       this.loginPromise = new Promise((resolve, reject) => {
-        this.ensureLoginHint().then(() => {
-          // Start the login flow
-          microsoftTeams.authentication.authenticate({
-            url: `${window.location.origin}/tab/silent-start`,
-            width: 600,
-            height: 535,
-            successCallback: () => {
-              resolve(this.getUser());
-            },
-            failureCallback: (reason) => {
-              reject(reason);
-            },
-          });
+        // Start the login flow
+        microsoftTeams.authentication.authenticate({
+          url: `${window.location.origin}/tab/silent-start`,
+          width: 600,
+          height: 535,
+          successCallback: () => {
+            resolve(this.getUser());
+          },
+          failureCallback: (reason) => {
+            reject(reason);
+          },
         });
       });
     }
@@ -67,54 +54,11 @@ class TeamsAuthService {
   }
 
   getUser() {
-    return new Promise((resolve, reject) => {
-      this.authContext.getUser((error, user) => {
-        if (!error) {
-          resolve(user.profile);
-        } else {
-          reject(error);
-        }
-      });
-    });
+    return Promise.resolve(this.app.getUser());
   }
 
   getToken() {
-    return new Promise((resolve, reject) => {
-      this.ensureLoginHint().then(() => {
-        this.authContext.acquireToken(
-          this.applicationConfig.endpoints.api,
-          (reason, token, error) => {
-            if (!error) {
-              resolve(token);
-            } else {
-              reject({ error, reason });
-            }
-          }
-        );
-      });
-    });
-  }
-
-  ensureLoginHint() {
-    return new Promise((resolve) => {
-      microsoftTeams.getContext((context) => {
-        const scopes = encodeURIComponent(
-          "email openid profile offline_access User.Read"
-        );
-
-        // Setup extra query parameters for ADAL
-        // - openid and profile scope adds profile information to the id_token
-        // - login_hint provides the expected user name
-        if (context.loginHint) {
-          this.authContext.config.extraQueryParameter = `prompt=consent&scope=${scopes}&login_hint=${encodeURIComponent(
-            context.loginHint
-          )}`;
-        } else {
-          this.authContext.config.extraQueryParameter = `prompt=consent&scope=${scopes}`;
-        }
-        resolve();
-      });
-    });
+    return this.app.acquireTokenSilent([this.applicationConfig.clientId]);
   }
 }
 
